e47deddb92dbb57f09490ba5538e6ecff5539078
OziboyDG <deguenonamen@gmail.com>
2023-11-02 12:16:47 +0100

    add space

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
+      debugShowCheckedModeBanner: false ,
       title: 'SimpliMoney_MTN',
       theme: ThemeData(  
         colorScheme: ColorScheme.fromSeed(seedColor: const Color.fromRGBO(253,203,12,5)),
