80cdcb58f8543f45579211379caeb34c6380f11c
OziboyDG <deguenonamen@gmail.com>
2023-11-25 00:09:15 +0100

    Fixed error

--- lib/screens/authentification/contact_page.dart
+++ lib/screens/authentification/contact_page.dart
@@ -43,139 +43,128 @@ class _ContactPageState extends State<ContactPage> {
         ),
         child: Scaffold(
           backgroundColor: Colors.transparent,
-          body: Stack(
+          body: ListView(
             children: [
-              Align(
-                alignment: Alignment.center,
-                child: Positioned(
-                    top: 80,
-                    child: Padding(
-                      padding: const EdgeInsets.all(2.0),
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: [
-                          Container(
-                            height: mediaSize.height * 0.5,
-                            width: mediaSize.width * 0.5,
-                            decoration: const BoxDecoration(
-                                image: DecorationImage(
-                                    image:
-                                        AssetImage("assets/SimpliMoney1.png"))),
-                          ),
-                        ],
-                      ),
-                    )),
-              ),
-              Positioned(
-                top: mediaSize.height * 0.35,
-                child: SizedBox(
-                  width: mediaSize.width,
-                  height: mediaSize.height * 0.4,
-                  child: Card(
-                    color: Colors.white60,
-                    shape: const RoundedRectangleBorder(
-                      borderRadius: BorderRadius.all(Radius.circular(30)),
+              Padding(
+                padding: const EdgeInsets.all(2.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Container(
+                      height: mediaSize.height * 0.3,
+                      width: mediaSize.width * 0.5,
+                      decoration: const BoxDecoration(
+                          image: DecorationImage(
+                              image:
+                                  AssetImage("assets/SimpliMoney1.png"))),
                     ),
-                    child: Padding(
-                      padding: const EdgeInsets.all(20.0),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          const Text(
-                            'Bienvenue !',
-                            style: TextStyle(
-                              fontSize: 30.0,
-                              fontWeight: FontWeight.bold,
-                            ),
+                  ],
+                ),
+              ),
+              SizedBox(
+                width: mediaSize.width,
+                height: mediaSize.height * 0.4,
+                child: Card(
+                  color: Colors.white60,
+                  shape: const RoundedRectangleBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(30)),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(20.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const Text(
+                          'Bienvenue !',
+                          style: TextStyle(
+                            fontSize: 30.0,
+                            fontWeight: FontWeight.bold,
                           ),
-                          SizedBox(
-                            height: mediaSize.height * 0.02,
+                        ),
+                        SizedBox(
+                          height: mediaSize.height * 0.02,
+                        ),
+                        const Text(
+                          "Veuillez entrer votre numéro Momo.",
+                          style: TextStyle(
+                            fontSize: 20.0,
+                            fontWeight: FontWeight.normal,
                           ),
-                          const Text(
-                            "Veuillez entrer votre numéro Momo.",
-                            style: TextStyle(
-                              fontSize: 20.0,
+                        ),
+                        SizedBox(
+                          height: mediaSize.height * 0.02,
+                        ),
+                        Form(
+                          child: IntlPhoneField(
+                            style: const TextStyle(
+                              fontSize: 25.0,
                               fontWeight: FontWeight.normal,
                             ),
+                            initialCountryCode: "BJ",
+                            decoration: InputDecoration(
+                                labelText: "Numéro Momo",
+                                border: OutlineInputBorder(
+                                    borderRadius: BorderRadius.circular(10))),
+                            controller: _mynumber,
                           ),
-                          SizedBox(
-                            height: mediaSize.height * 0.02,
-                          ),
-                          Form(
-                            child: IntlPhoneField(
-                              style: const TextStyle(
-                                fontSize: 25.0,
-                                fontWeight: FontWeight.normal,
+                        ),
+                        InkWell(
+                          onTap: () async {
+                            print(_mynumber.text);
+                            number = _mynumber.text;
+                            if (number!.isNotEmpty && number!.length == 8) {
+                              SharedPreferences pref =
+                                  await SharedPreferences.getInstance();
+                              pref.setString('NUMBER', number!);
+                              //FIXME: get availableAmount from the API and setit here
+                              pref.setInt('availableAmount', 20000);
+                              pref.setInt('thriftAmount', 0);
+                              Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) => PinPage(
+                                            number: number!,
+                                          )));
+                            }
+                          },
+                          child: Center(
+                            child: Container(
+                              width: mediaSize.width * 0.3,
+                              padding:
+                                  const EdgeInsets.symmetric(vertical: 10.0),
+                              decoration: BoxDecoration(
+                                color: Colors.green,
+                                borderRadius: BorderRadius.circular(10.0),
                               ),
-                              initialCountryCode: "BJ",
-                              decoration: InputDecoration(
-                                  labelText: "Numéro Momo",
-                                  border: OutlineInputBorder(
-                                      borderRadius: BorderRadius.circular(10))),
-                              controller: _mynumber,
-                            ),
-                          ),
-                          InkWell(
-                            onTap: () async {
-                              print(_mynumber.text);
-                              number = _mynumber.text;
-                              if (number!.isNotEmpty && number!.length == 8) {
-                                SharedPreferences pref =
-                                    await SharedPreferences.getInstance();
-                                pref.setString('NUMBER', number!);
-                                //FIXME: get availableAmount from the API and setit here
-                                pref.setInt('availableAmount', 20000);
-                                pref.setInt('thriftAmount', 0);
-                                Navigator.push(
-                                    context,
-                                    MaterialPageRoute(
-                                        builder: (context) => PinPage(
-                                              number: number!,
-                                            )));
-                              }
-                            },
-                            child: Center(
-                              child: Container(
-                                width: mediaSize.width * 0.3,
-                                padding:
-                                    const EdgeInsets.symmetric(vertical: 10.0),
-                                decoration: BoxDecoration(
-                                  color: Colors.green,
-                                  borderRadius: BorderRadius.circular(10.0),
-                                ),
-                                child: const Text(
-                                  'Validez',
-                                  textAlign: TextAlign.center,
-                                  style: TextStyle(
-                                    fontWeight: FontWeight.bold,
-                                    fontSize: 12,
-                                  ),
+                              child: const Text(
+                                'Validez',
+                                textAlign: TextAlign.center,
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 12,
                                 ),
                               ),
                             ),
-                          )
-                        ],
-                      ),
+                          ),
+                        )
+                      ],
                     ),
                   ),
                 ),
               ),
-              Positioned(
-                  bottom: mediaSize.height * 0.03,
-                  right: mediaSize.width * 0.02,
-                  child: ElevatedButton(
-                    onPressed: () async {
-                      print('On Tap');
-                      player.stop();
-                      print('End Stop');
-                      player.setAsset('assets/audios/Phrase_1.mp3');
-                      player.play();
-                      print('Edd Play');
-                    },
-                    child: const Icon(Icons.volume_up),
-                  ))
             ],
+          ),
+          floatingActionButton: ElevatedButton(
+            onPressed: () async {
+              print('On Tap');
+              player.stop();
+              print('End Stop');
+              player.setAsset('assets/audios/Phrase_1.mp3');
+              player.play();
+              print('Edd Play');
+            },
+            child: const Icon(Icons.volume_up),
           ),
         ),
       ),
